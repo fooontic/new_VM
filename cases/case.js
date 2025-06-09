243e03548249d2fad827e266f79c1941f1726475
fooontic <fooontic@gmail.com>
2025-06-09 17:36:02 +0200

    add case footer

--- cases/case.js
+++ cases/case.js
@@ -1,7 +1,9 @@
 import { initFloatingToc } from "/scripts/toc.js";
 import { initScrollBgRepaint } from "/scripts/caseBgRepaint.js";
+import { initPlane } from "/scripts/footer.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     initFloatingToc();
     initScrollBgRepaint();
+    initPlane(".footer__chat", ".footer__plane");
 });
